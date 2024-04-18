bf56fa7341c704d3a76a7f774cd972844b6fb98c
vedantpatel1997 <vedantp9@gmail.com>
2024-04-17 21:13:50 -0600

    QA CHANGE

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>REACT APP VEDANT PATEL</h1>
+        <h1>REACT APP VEDANT PATEL QUALITY</h1>
         <p>Hosted on Linux</p>
         <p>VPReactAppLinux</p>
       </header>
