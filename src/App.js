5726a8126c94620604cedebbb7bae86622fa2f78
vedantpatel1997 <vedantp9@gmail.com>
2024-04-17 21:11:37 -0600

    INIT

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>REACT APP</h1>
+        <h1>REACT APP VEDANT PATEL</h1>
         <p>Hosted on Linux</p>
         <p>VPReactAppLinux</p>
       </header>
